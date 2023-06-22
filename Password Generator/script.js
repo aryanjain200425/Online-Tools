14823ad6046ac595138e46be71153da8b06d4c56
Aryan Jain <aryanrashijain@gmail.com>
2023-06-22 12:32:40 -0700

    added password length option

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,16 +1,18 @@
+const numberDropdown = document.getElementById('numberDropdown');
 const generateButton = document.getElementById('generateButton');
 const passwordInput = document.getElementById('passwordInput');
-
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+';
-const passwordLength = 12;
 
 generateButton.addEventListener('click', generatePassword);
 
 function generatePassword() {
+  const selectedNumber = parseInt(numberDropdown.value);
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+';
   let password = '';
-  for (let i = 0; i < passwordLength; i++) {
+  
+  for (let i = 0; i < selectedNumber; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     password += characters[randomIndex];
   }
+  
   passwordInput.value = password;
 }
